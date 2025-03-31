3a748126f05584e8b40db7761169060f153affb4
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-31 15:09:30 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 component =frontend
+logfile=/tmp/$component.log
 echo -n "installing ngnix"
-dnf install nginx -y   &>> /tmp/component.log
+dnf install nginx -y   &>> $logfile
 if [ $? -eq 0 ]; then
   echo -e "\e[32m success \e[0m"
   else
     echo -e "\e[31m failure \e[0m"
 fi
-systemctl enable nginx &>> /tmp/component.log
+systemctl enable nginx &>> $logfile
 echo -n "starting ngnix"
-systemctl start nginx  &>> /tmp/component.log
+systemctl start nginx  &>> $logfile
 if [ $? -eq 0 ]; then
   echo -e "\e[32m success \e[0m"
   else
@@ -24,7 +25,7 @@ if [ $? -eq 0 ]; then
     echo -e "failure"
   fi
 echo -n "downloading component content"
-curl -o /tmp/component.zip https://expense-web-app.s3.amazonaws.com/component.zip
+curl -o /tmp/component.zip https://expense-web-app.s3.amazonaws.com/component.zip &>> $logfile
 if [ $? -eq 0 ]; then
   echo -e "\e[32m success \e[0m"
   else
@@ -32,7 +33,7 @@ if [ $? -eq 0 ]; then
   fi
 echo -n "extracting component content"
 cd /usr/share/nginx/html 
-unzip -o /tmp/component.zip &>> /tmp/component.log
+unzip -o /tmp/component.zip &>> $logfile
 if [ $? -eq 0 ]; then
   echo -e "\e[32m success \e[0m"
   else
