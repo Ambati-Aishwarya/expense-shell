e5cce1fb59cb431cf73af2f388abc5d7b362b713
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-31 15:17:39 +0530

    frontend

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ golden rules to be followed when automating the manual process
 4. when you rerun the script from the second time on the same server, it should be qualified.
 Areas of improvements
 1.if condition is used multiple times ,let's try to put it as a function and call that as needed
+DRY(Dont repeat yourself)WET(Write everything twice)are software engineering principles that describe how to write code.Dry code is prefered because its easier to maintain and scale
--- frontend.sh
+++ frontend.sh
@@ -4,7 +4,7 @@ component =frontend
 logfile=/tmp/$component.log
 echo -n "installing ngnix"
 dnf install nginx -y   &>> $logfile
-if [ $? -eq 0 ]; then
+if [ $1 -eq 0 ]; then
   echo -e "\e[32m success \e[0m"
   else
     echo -e "\e[31m failure \e[0m"
@@ -12,34 +12,18 @@ fi
 systemctl enable nginx &>> $logfile
 echo -n "starting ngnix"
 systemctl start nginx  &>> $logfile
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-  else
-    echo -e "failure"
-  fi
+stat $?
 echo -n "clearing old web content:"
 rm -rf /user/share/nginx/html/*
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-  else
-    echo -e "failure"
-  fi
+stat $?
 echo -n "downloading component content"
 curl -o /tmp/component.zip https://expense-web-app.s3.amazonaws.com/component.zip &>> $logfile
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-  else
-    echo -e "failure"
-  fi
+stat $?
 echo -n "extracting component content"
 cd /usr/share/nginx/html 
 unzip -o /tmp/component.zip &>> $logfile
-if [ $? -eq 0 ]; then
-  echo -e "\e[32m success \e[0m"
-  else
-    echo -e "failure"
-  fi
+stat $?
 echo -n "restarting nginix"
 systemctl restart nginx 
-
+stat $?
 echo -n "component executed sucessfully"
