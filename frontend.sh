85c83033966eb51b0f225ead896cc4120e060b11
Ambati07 <ambatiaishwarya@gmail.com>
2025-03-31 13:41:39 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo -n "installing ngnix"
+echo -e "installing ngnix"
 dnf install nginx -y   &>> /tmp/frontend.log
-echo -n "enabling ngnix"
+echo -e "enabling ngnix"
 systemctl enable nginx &>> /tmp/frontend.log
-echo -n "starting ngnix"
+echo -e "starting ngnix"
 systemctl start nginx  &>> /tmp/frontend.log
 # rm -rf /usr/share/nginx/html/* 
 #curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
